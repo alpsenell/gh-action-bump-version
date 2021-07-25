1454e4603934707a41c21df83696d212f220404b
alpsenell <alp@useinsider.com>
2021-07-25 15:15:53 +0300

    trying new action

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ Toolkit.run(async (tools) => {
   console.log('package:', pkg);
   console.log('event:', event);
 
-  if (!event.commits) {
-    console.log("Couldn't find any commits in this event, incrementing patch version...");
-  }
+  const currentVersion = pkg.version.toString();
+  const currentMajorVersion = currentVersion.split('.')[0];
+  const currentMinorVersion = currentVersion.split('.')[1];
+  const currentPatchVersion = currentVersion.split('.')[2];
 
   const tagPrefix = process.env['INPUT_TAG-PREFIX'] || '';
   const messages = event.commits ? event.commits.map((commit) => commit.message + '\n' + commit.body) : [];
@@ -30,39 +31,29 @@ Toolkit.run(async (tools) => {
     return;
   }
 
-  // input wordings for MAJOR, MINOR, PATCH, PRE-RELEASE
   const majorWords = process.env['INPUT_MAJOR-WORDING'].split(',');
   const minorWords = process.env['INPUT_MINOR-WORDING'].split(',');
-  // patch is by default empty, and '' would always be true in the includes(''), thats why we handle it separately
   const patchWords = process.env['INPUT_PATCH-WORDING'] ? process.env['INPUT_PATCH-WORDING'].split(',') : null;
   const preReleaseWords = process.env['INPUT_RC-WORDING'].split(',');
 
   console.log('config words:', { majorWords, minorWords, patchWords, preReleaseWords });
 
-  // get default version bump
   let version = process.env.INPUT_DEFAULT;
   let foundWord = null;
-  // get the pre-release prefix specified in action
-  let preid = process.env.INPUT_PREID;
 
-  // case: if wording for MAJOR found
-  if (
+  if (currentPatchVersion == '99') {
+    version = 'minor';
+  } else if (
     messages.some(
       (message) => /^([a-zA-Z]+)(\(.+\))?(\!)\:/.test(message) || majorWords.some((word) => message.includes(word)),
     )
   ) {
     version = 'major';
-  }
-  // case: if wording for MINOR found
-  else if (messages.some((message) => minorWords.some((word) => message.includes(word)))) {
+  } else if (messages.some((message) => minorWords.some((word) => message.includes(word)))) {
     version = 'minor';
-  }
-  // case: if wording for PATCH found
-  else if (patchWords && messages.some((message) => patchWords.some((word) => message.includes(word)))) {
+  } else if (patchWords && messages.some((message) => patchWords.some((word) => message.includes(word)))) {
     version = 'patch';
-  }
-  // case: if wording for PRE-RELEASE found
-  else if (
+  } else if (
     messages.some((message) =>
       preReleaseWords.some((word) => {
         if (message.includes(word)) {
@@ -74,42 +65,14 @@ Toolkit.run(async (tools) => {
       }),
     )
   ) {
-    preid = foundWord.split('-')[1];
     version = 'prerelease';
   }
 
-  console.log('version action after first waterfall:', version);
-
-  // case: if default=prerelease,
-  // rc-wording is also set
-  // and does not include any of rc-wording
-  // then unset it and do not run
-  if (
-    version === 'prerelease' &&
-    preReleaseWords !== '' &&
-    !messages.some((message) => preReleaseWords.some((word) => message.includes(word)))
-  ) {
-    version = null;
-  }
-
-  // case: if default=prerelease, but rc-wording is NOT set
-  if (version === 'prerelease' && preid) {
-    version = 'prerelease';
-    version = `${version} --preid=${preid}`;
-  }
-
-  console.log('version action after final decision:', version);
+  console.log('version action decision:', version);
 
   // case: if nothing of the above matches
   if (version === null) {
     tools.exit.success('No version keywords found, skipping bump.');
-    return;
-  }
-
-  // case: if user sets push to false, to skip pushing new tag/package.json
-  const push = process.env['INPUT_PUSH']
-  if ( push === "false" || push === false ) {
-    tools.exit.success('User requested to skip pushing new tag and package.json. Finished.');
     return;
   }
 
